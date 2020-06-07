eeeceab5f2a915744551217730067f656d5ace8f
Oroden <jimmykasprzak@gmail.com>
2020-06-07 22:47:56 +0200

    :sparkles: first draft about me + align about me and personal info section

---
+++ components/AboutMe.vue
@@ -0,0 +1,42 @@
+<template>
+  <section>
+    <h2>About me</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui saepe rem temporibus facere laudantium commodi voluptates mollitia reprehenderit nisi autem? Exercitationem optio architecto assumenda cum consequuntur praesentium rem atque itaque.</p>
+
+    <div class="qualities">
+      <div class="quality">
+        <i class="material-icons">home</i>
+        <div class="quality-content">
+          <h3>Creative</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit tenetur dolor, minima accusamus error est at natus velit eveniet! Cupiditate est maxime aspernatur numquam quos minima dolore rerum delectus doloremque?</p>
+        </div>
+      </div>
+
+      <div class="quality">
+        <i class="material-icons">home</i>
+        <div class="quality-content">
+          <h3>Creative</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit tenetur dolor, minima accusamus error est at natus velit eveniet! Cupiditate est maxime aspernatur numquam quos minima dolore rerum delectus doloremque?</p>
+        </div>
+      </div>
+
+      <div class="quality">
+        <i class="material-icons">home</i>
+        <div class="quality-content">
+          <h3>Creative</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit tenetur dolor, minima accusamus error est at natus velit eveniet! Cupiditate est maxime aspernatur numquam quos minima dolore rerum delectus doloremque?</p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.qualities {
+  display: flex;
+}
+
+.quality {
+  display: flex;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,41 @@
 <template>
   <section>
-    <Header/>
+    <Header />
+    <section class="row-2-1">
+      <AboutMe class="about-me" />
+      <div class="personal-info">
+        Personal Info incoming
+      </div>
+    </section>
   </section>
 </template>
 
 <script>
 import Header from "~/components/Header.vue";
+import AboutMe from "~/components/AboutMe.vue";
 
 export default {
   components: {
-    Header
+    Header,
+    AboutMe
   }
 };
 </script>
 
 <style scoped>
 section {
+  flex: 1;
+}
+
+.row-2-1 {
+  display: flex;
+}
+
+.about-me {
+  flex: 2;
+}
+
+.personal-info {
   flex: 1;
 }
 </style>
