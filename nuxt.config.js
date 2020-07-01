a43b7edb5d1d751ddc5809782d96085a1c2e02cd
Oroden <jimmykasprzak@gmail.com>
2020-07-01 23:09:03 +0200

    :wrench: modify title

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ export default {
   ** Doc: https://vue-meta.nuxtjs.org/api/#metainfo-properties
   */
   head: {
-    title: 'Nuxt.js starter for CSB',
+    title: 'Jimmy Kasprzak&',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
