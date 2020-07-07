5dcb60bedb0346e1f6a65771ad928dccefefb84b
Oroden <jimmykasprzak@gmail.com>
2020-07-07 23:17:15 +0200

    :sparkles: add self-intro to about-me page

--- pages/about-me.vue
+++ pages/about-me.vue
@@ -1,6 +1,23 @@
 <template>
-  <Header />
+  <section>
+    <Header />
+
+    <section class="self-intro">
+      Hi, I’m Jimmy. I’m an French software engineer living and working in Nantes, France.
+      <br/>
+      I'm also a trainer and I love sharing my passion for software development with people in many ways like writing blogs and speaking at meetups and conferences.
+    </section>
+  </section>
 </template>
+
+<style scoped>
+.self-intro {
+  padding: 60px 40px;
+  color: #fff;
+  font-size: 26px;
+  background-color: var(--blue);
+}
+</style>
 
 <script>
 import Header from "~/components/Header.vue";
