9a546deb21a6072c994861b28e3b76ba8bf8dfcb
Oroden <jimmykasprzak@gmail.com>
2020-07-16 17:30:46 +0200

    :sparkles: add skills page intro

--- pages/skills.vue
+++ pages/skills.vue
@@ -1,3 +1,16 @@
 <template>
-  <h2>Skills</h2>
+  <section class="skills-intro">
+    In this page, you can find tech skills I like to use at the moment.
+    <br />
+    This is non-exhaustive, completly subjective, but it allows you to know very quickly what my tech interests and competences are at the moment.
+  </section>
 </template>
+
+<style scoped>
+.skills-intro {
+  padding: 60px 40px;
+  color: #fff;
+  font-size: 26px;
+  background-color: var(--orange);
+}
+</style>
