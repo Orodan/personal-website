57ed4e04efaa7a79bf052b0b1bb7f281bcb16b52
Oroden <jimmykasprzak@gmail.com>
2020-07-18 23:05:39 +0200

    :sparkles: add basic layout and content to contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,3 +1,83 @@
 <template>
-  <h2>Contact</h2>
+  <section class="contact">
+    <section class="left">
+      <img src="~/assets/tiny-rick.jpg" alt="profile picture">
+    </section>
+
+    <section class="right">
+      <ul class="labels">
+        <li><strong>Mail:</strong></li>
+        <li><strong>Twitter:</strong></li>
+        <li><strong>Linkedin:</strong></li>
+        <li><strong>Github:</strong></li>
+      </ul>
+      <ul class="values">
+        <li><span>john.doe@gmail.com</span></li>
+        <li><span>Orodan</span></li>
+        <li><span>Jimmy Kasprak</span></li>
+        <li><span>Oroden</span></li>
+      </ul>
+    </section>
+
+  </section>
 </template>
+
+<style scoped>
+.contact {
+  background-color: var(--white);
+  padding: 40px;
+  display: flex;
+}
+
+.left {
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.right {
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.right > * {
+  flex: 1;
+}
+
+ul {
+  list-style: none;
+  padding: 0;
+}
+
+li {
+  width: 100%;
+  padding: 10px;
+}
+
+.labels li {
+  text-align: left;
+  padding-left: 50%;
+}
+
+.values li {
+  text-align: left;
+}
+
+.right > div:first-child strong {
+  text-align: left;
+}
+
+strong, span {
+  padding: 10px;
+  font-size: 18px;
+}
+
+img {
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+}
+</style>
