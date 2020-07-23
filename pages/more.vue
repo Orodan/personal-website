6abc309b2e4c5b32d91caeed3141623844baf632
Oroden <jimmykasprzak@gmail.com>
2020-07-23 21:24:49 +0200

    :sparkles: add more page intro

--- pages/more.vue
+++ pages/more.vue
@@ -1,3 +1,14 @@
 <template>
-  <h2>More</h2>
+  <section class="more-intro">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, dolore et. Explicabo at sequi eius numquam veritatis, necessitatibus iste consectetur distinctio sunt eum laborum illo eaque assumenda quo culpa illum!
+  </section>
 </template>
+
+<style scoped>
+.more-intro {
+  padding: 60px 40px;
+  color: #fff;
+  font-size: 26px;
+  background-color: var(--yellow);
+}
+</style>
