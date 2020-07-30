a9f8f0513d55f7d3ceeb72331fe21ebb890af697
Oroden <jimmykasprzak@gmail.com>
2020-07-30 22:07:52 +0200

    :sparkles: add link to come back from article to article links

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,7 +1,15 @@
 <template>
-  <article>
-    <nuxt-content :document="article" />
-  </article>
+  <section>
+    <section class="go-back">
+      <nuxt-link to="/more" class="articles-link">
+        <i class="material-icons">keyboard_backspace</i> Retour aux articles
+      </nuxt-link>
+    </section>
+
+    <article>
+      <nuxt-content :document="article" />
+    </article>
+  </section>
 </template>
 
 <script>
@@ -18,5 +26,28 @@ export default {
 article {
   padding: 60px 40px;
   background-color: var(--white);
+}
+
+.go-back {
+  padding: 40px;
+  background-color: var(--white);
+  border-bottom: 1px solid var(--yellow);
+}
+
+.articles-link {
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  color: black;
+  font-size: 20px;
+  transition: color .3s;
+}
+
+.articles-link > i {
+  margin-right: 20px;
+}
+
+.articles-link:hover {
+  color: var(--yellow);
 }
 </style>
