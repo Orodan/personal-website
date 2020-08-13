68530560c37998c3e1ff03ce06c7ec0c90880c31
Oroden <jimmykasprzak@gmail.com>
2020-08-13 23:42:03 +0200

    :fire: remove useless code

--- components/AboutMe.vue
+++ components/AboutMe.vue
@@ -10,34 +10,6 @@
 
       <img :src="articleSummary.img" :alt="articleSummary.title" />
     </section>
-
-    <!-- <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil soluta, dolor, maxime sit necessitatibus facere possimus distinctio atque eos quidem doloremque iure.</p>
-
-    <!-- <div class="qualities">
-      <div class="quality">
-        <i class="material-icons md-36 yellow">spa</i>
-        <div class="quality-content">
-          <h3 class="yellow">Curious</h3>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et tempore non, sit id minima veniam! Quasi, commodi impedit necessitatibus</p>
-        </div>
-      </div>
-
-      <div class="quality">
-        <i class="material-icons md-36 green">favorite</i>
-        <div class="quality-content">
-          <h3 class="green">Passionate</h3>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, qui eos illo ipsum cumque nemo temporibus quibusdam laborum possimus</p>
-        </div>
-      </div>
-
-      <div class="quality">
-        <i class="material-icons md-36 red">camera</i>
-        <div class="quality-content">
-          <h3 class="red">Meticulous</h3>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat perferendis molestias a dolor dicta accusantium eum, eius tenetur</p>
-        </div>
-      </div>
-    </div> -->
   </section>
 </template>
 
@@ -74,9 +46,14 @@ h3 {
 
 p {
   margin-bottom: 20px;
+  font-size: 16px;
   flex: 1;
   display: flex;
   align-items: center;
+}
+
+img {
+  width: 200px;
 }
 
 .article-summary {
@@ -89,34 +66,5 @@ p {
 .article-summary-content {
   display: flex;
   flex-direction: column;
-}
-
-.article-summary-content p {
-  font-size: 18px;
-}
-
-.article-summary img {
-  width: 200px;
-}
-
-.qualities {
-  display: flex;
-}
-
-.quality {
-  display: flex;
-  margin: 0 20px;
-}
-
-.quality:first-child {
-  margin-left: 0;
-}
-
-.quality:last-child {
-  margin-right: 0;
-}
-
-i {
-  margin-right: 20px;
 }
 </style>
