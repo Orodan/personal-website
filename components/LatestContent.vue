b68484565190800678e45a8feac0fff98c95265d
Oroden <jimmykasprzak@gmail.com>
2020-09-12 22:26:14 +0200

    :sparkles: allow to click on latest content on home page to go directly to article

--- components/LatestContent.vue
+++ components/LatestContent.vue
@@ -2,7 +2,7 @@
   <section class="about-me">
     <h2 class="blue">Latest content</h2>
 
-    <section class="article-summary">
+    <section class="article-summary" @click="goTo(articleSummary.slug)">
       <div class="article-summary-content">
         <h3 class="green">{{ articleSummary.title }}</h3>
         <p>{{ articleSummary.description }}</p>
@@ -20,7 +20,12 @@ export default {
       type: Object,
       default: () => ({})
     }
-  }
+  },
+  methods: {
+    goTo: function (slug) {
+      this.$router.push({ name: 'blog-slug', params: { slug } })
+    }
+  },
 }
 </script>
 
@@ -61,6 +66,12 @@ img {
   box-shadow: 0 1px 3px 0 rgba(0,0,0,.3);
   border-radius: 5px;
   padding: 20px;
+  cursor: pointer;
+  transition: all .3s;
+}
+
+.article-summary:hover {
+  box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
 }
 
 .article-summary-content {
